0ca9002ec8ab220a91f60b7f679d00cce0fb6f93
Supun Wimalasena <supun@hyvor.com>
2025-02-11 18:28:36 +0100

    phpstan, test fixes

--- src/Exception/ClickhousePingException.php
+++ src/Exception/ClickhousePingException.php
@@ -4,7 +4,7 @@ namespace Hyvor\Clickhouse\Exception;
 
 class ClickhousePingException extends ClickhouseException
 {
-    public function __construct(string $message = 'Clickhouse ping failed', int $code = 0, \Throwable $previous = null)
+    public function __construct(string $message = 'Clickhouse ping failed', int $code = 0, ?\Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
